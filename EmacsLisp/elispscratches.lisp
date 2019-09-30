41389d8d71495f32975e5a9d869edf1ff103213b
Chun-Yen Wang <cywang@Chun-Yens-iMac.local>
2019-09-30 07:45:52 -0700

    ASC2 ops re-cap

--- EmacsLisp/elispscratches.lisp
+++ EmacsLisp/elispscratches.lisp
@@ -1,4 +1,6 @@
 
+;; [Emacs Lisp: getting ascii value of character](https://stackoverflow.com/questions/19862517/emacs-lisp-getting-ascii-value-of-character)
+
 ?Q
 ;; 81 (#o121, #x51, ?Q)
 
@@ -7,3 +9,11 @@
 
 ?\a
 ;; 7 (#o7, #x7, ?\C-g)
+
+?a
+;; 97 (stupid i forgot bout this)
+(- ?x ?d)
+;; 20
+
+(- ?a ?A)
+;; 32 (why??)
